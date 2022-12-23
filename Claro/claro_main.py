e238c4a3372e3634b4aff4b9926fee871bc5e2c0
Jacopo Altieri <jacopo.altieri@gmail.com>
2022-12-23 12:07:55 +0100

    first commit of main: work on hardcoded single file

---
+++ Claro/claro_main.py
@@ -0,0 +1,31 @@
+import claro_class as cl
+import numpy as np
+import pandas as pd
+import os
+import fnmatch
+import sys
+import matplotlib.pyplot as plt
+
+
+# Leave hardcoded = true to test the program on a single file, just for now
+hardcoded = True
+path = r'C:\Users\jacop\Desktop\OOP\Ch_7_offset_0_Chip_004.txt'
+
+if hardcoded != True:
+    # check if path has been given
+        if len(sys.argv) !=2:
+            print("\nUsage: insert a valid directory or filename\n")
+        sys.exit(1)
+
+        #path = sys.argv[1]
+
+        def isFile(path):
+            filename = "Ch_?_offset_?_Chip_00?"
+            return fnmatch.fnmatch(path, filename) # True
+
+
+prova = cl.Single(path)
+prova.fit_erf()
+prova.printData()
+
+
